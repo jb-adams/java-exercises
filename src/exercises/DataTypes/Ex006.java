5081b3aeeed27a8d57f37cb816f4a9b225ca1020
Jeremy Adams <jeremybr.adams@gmail.com>
2018-03-14 19:46:31 -0400

    finished data types exercise 006

--- src/exercises/DataTypes/Ex006.java
+++ src/exercises/DataTypes/Ex006.java
@@ -7,7 +7,29 @@ import java.util.Scanner;
 
 public class Ex006 {
 
+    private static double computeBMI(double weight, double height) {
+        return ((weight / (height * height)) * 703.0);
+    }
+
     public static void main(String[] args) {
+        double weight;
+        double height;
+        double bmi;
+        String output = new String();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input weight in pounds: ");
+        weight = sc.nextDouble();
+
+        System.out.println("Input height in inches: ");
+        height = sc.nextDouble();
+
+        bmi = computeBMI(weight, height);
+
+        output = "weight in pounds = " + weight + "\n"
+               + "height in inches = " + height + "\n"
+               + "BMI = " + bmi + "\n";
+        System.out.println(output);
 
     }
 }
